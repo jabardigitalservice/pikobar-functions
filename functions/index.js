13beb7de2a11b62c2256947b6380b6144bdc06d0
Yoga Hanggara <yohang88@gmail.com>
2020-03-25 14:32:03 +0700

    function create user on authenticated
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,41 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
+});
+
+exports.storeUser = functions.auth.user().onCreate((user) => {
+  console.log('User:', user);
+
+  var record = user.toJSON()
+  record.createdAt = admin.firestore.Timestamp.now();
+
+  admin.firestore()
+    .collection(`users`)
+    .doc(user.uid)
+    .set(record);
+});
+
+exports.deleteUser = functions.auth.user().onDelete((user) => {
+  console.log('User:', user);
+
+  admin.firestore()
+    .collection(`users`)
+    .doc(user.uid)
+    .delete();
+});
+
+exports.autoSubscribeTopic = functions.firestore.document(`tokens/{tokenId}`).onCreate(async (snap, context) => {
+  var tokenId = context.params.tokenId;
+
+  var token = snap.data();
+  console.log('Token:', token);
+
+  admin.messaging().subscribeToTopic(tokenId, 'general');
 });
