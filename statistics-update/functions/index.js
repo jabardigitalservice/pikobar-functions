c4c4f56b608179677f29ec95b605c4d4375724fb
Aldi Doanta <aldidkurnia@gmail.com>
2020-03-26 12:58:10 +0700

    [Statistics] Add logic to update data

--- statistics-update/functions/index.js
+++ statistics-update/functions/index.js
@@ -7,7 +7,45 @@ admin.initializeApp();
 exports.updateStatistics = functions.https.onRequest((request, response) => {
   axios.get('https://covid19-public.digitalservice.id/api/v1/rekapitulasi/jabar')
         .then(function (res) {
-            const data = res.data;
+            const data = res.data.data.content;
+
+            const lastUpdate = res.data.data.metadata.last_update ?
+              res.data.data.metadata.last_update :
+              Math.floor(Date.now() / 1000);
+
+            // console.log(data);
+            const updatedStats = {
+              'aktif': {
+                'jabar': data.positif
+              },
+              'sembuh': {
+                'jabar': data.sembuh
+              },
+              'meninggal': {
+                'jabar': data.meninggal
+              },
+              'odp': {
+                'selesai': {
+                  'jabar': data.odp_selesai
+                },
+                'total': {
+                  'jabar': data.odp_total
+                }
+              },
+              'pdp': {
+                'selesai': {
+                  'jabar': data.pdp_selesai
+                },
+                'total': {
+                  'jabar': data.pdp_total
+                }
+              }
+            };
+
+            admin.firestore()
+            .collection('statistics')
+            .doc('test-cloud-function')
+            .set(updatedStats, {merge: true});
 
             response.json({"isSuccess": true});
         })
