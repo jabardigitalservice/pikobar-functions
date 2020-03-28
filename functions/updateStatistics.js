cb089eabe86e40ba1fc0ea5a6375b78854b46d65
Aldi Doanta <aldidkurnia@gmail.com>
2020-03-28 23:10:21 +0700

    [Statistics] Add logic to update national stats, add env vars

--- functions/updateStatistics.js
+++ functions/updateStatistics.js
@@ -5,50 +5,73 @@ const axios = require('axios').default;
 exports.updateStatistics =
   functions.pubsub.schedule('every 30 minutes')
     .timeZone('Asia/Jakarta')
-    .onRun((context) => {
-      return axios.get(functions.config().env.updateStatistics.apiURL)
-            .then(res => {
-                const data = res.data.data.content;
-                let lastUpdate = res.data.data.metadata.last_update;
-                const milliseconds = lastUpdate ? new Date(lastUpdate).getTime() : Date.now();
-                lastUpdate = new admin.firestore.Timestamp(Math.floor(milliseconds / 1000), 0);
-                const updatedStats = {
-                  'updated_at': lastUpdate,
-                  'aktif': {
-                    'jabar': data.positif
-                  },
-                  'sembuh': {
-                    'jabar': data.sembuh
-                  },
-                  'meninggal': {
-                    'jabar': data.meninggal
-                  },
-                  'odp': {
-                    'selesai': {
-                      'jabar': data.odp_selesai
-                    },
-                    'total': {
-                      'jabar': data.odp_total
-                    }
-                  },
-                  'pdp': {
-                    'selesai': {
-                      'jabar': data.pdp_selesai
-                    },
-                    'total': {
-                      'jabar': data.pdp_total
-                    }
-                  }
-                };
-
-                admin.firestore()
-                .collection('statistics')
-                .doc('jabar-dan-nasional')
-                .set(updatedStats, {merge: true});
-
-                return 0;
-            })
-            .catch(error => {
-                console.log(error);
-            })
+    .onRun(async context => {
+      let statistics = {
+        'updated_at': null,
+        'aktif': {},
+        'sembuh': {},
+        'meninggal': {},
+        'odp': {
+          'selesai': {},
+          'total': {}
+        },
+        'pdp': {
+          'selesai': {},
+          'total': {}
+        }
+      };
+      statistics = await getJabarStatistics(statistics);
+      statistics = await getNationalStatistics(statistics);
+      updateStatistics(statistics);
+      console.log('finish updating statistics')
+      response.send('ok');
     });
+
+
+async function getJabarStatistics(updatedStatistics) {
+  return axios.get(functions.config().env.updateStatistics.apiURL)
+    .then(res => {
+        const data = res.data.data.content;
+        let lastUpdate = res.data.data.metadata.last_update;
+        const milliseconds = lastUpdate ? new Date(lastUpdate).getTime() : Date.now();
+        lastUpdate = new admin.firestore.Timestamp(Math.floor(milliseconds / 1000), 0);
+
+        updatedStatistics.updated_at = lastUpdate;
+        updatedStatistics.aktif.jabar = data.positif;
+        updatedStatistics.sembuh.jabar = data.sembuh;
+        updatedStatistics.meninggal.jabar = data.meninggal;
+        updatedStatistics.odp.selesai.jabar = data.odp_selesai;
+        updatedStatistics.odp.total.jabar = data.odp_total;
+        updatedStatistics.pdp.selesai.jabar = data.pdp_selesai;
+        updatedStatistics.pdp.total.jabar = data.pdp_total;
+
+        return updatedStatistics;
+    })
+    .catch(error => {
+        console.log(error);
+        return (error);
+    })
+}
+
+async function getNationalStatistics(updatedStatistics) {
+  return axios.get('https://indonesia-covid-19.mathdro.id/api')
+    .then(res => {
+        const data = res.data;
+        updatedStatistics.aktif.nasional = data.jumlahKasus;
+        updatedStatistics.sembuh.nasional = data.sembuh;
+        updatedStatistics.meninggal.nasional = data.meninggal;
+
+        return updatedStatistics;
+    })
+    .catch(error => {
+        console.log(error);
+        return (error);
+    })
+}
+
+function updateStatistics(statistics) {
+  admin.firestore()
+    .collection('statistics')
+    .doc('test-functions')
+    .set(statistics, {merge: true});
+}
