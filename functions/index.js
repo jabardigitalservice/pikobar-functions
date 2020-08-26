08e037d6545d21e0dc64ea3eaf26f181cabd4159
Risa Herdianto <aputra.zha@gmail.com>
2020-08-26 10:12:20 +0700

    Added a function to update user data in the tracking dashboard

--- functions/index.js
+++ functions/index.js
@@ -136,6 +136,45 @@ exports.autoSubscribeTopic = functions.firestore.document(`tokens/{tokenId}`).on
   return 'ok';
 });
 
+/**
+ * Update user data in the tracking dashboard 
+ * whenever user data in firestore is updated.
+ */
+exports.updateProfileTracking = functions.firestore.document('users/{userId}').onUpdate( async (change, context) => {
+
+  // Get the tracking profile url from the environment config
+  const profileUrl = functions.config().env.tracking.profileurl;
+
+  // Get the parameter `{userId}`
+  const userId = context.params.userId;
+
+  // Get an object representing the user document
+  const newValue = change.after.data();
+
+  // Set the data to be sent when performing a PATCH request
+  const record = {
+    userId: userId,
+    name: newValue.name,
+    gender: String(newValue.gender),
+    stay_at: String(newValue.address),
+    phone: String(newValue.phone_number),
+    stay_at_province: String(newValue.province_id),
+    stay_at_city: String(newValue.city_id),
+    birthday: (typeof newValue.birthdate !== 'undefined' && newValue.birthdate) ? newValue.birthdate.toDate().toISOString() : 'undefined',
+    status: String(newValue.health_status)
+  }
+
+  // Performs a PATCH request to update user profile data in the tracking dashboard
+  await axios({
+    method: 'PATCH',
+    url: profileUrl,
+    data: record
+  }).then(response => console.log(response.status))
+  .catch(error => console.error(error));
+
+  return 'OK';
+});
+
 exports.updateStatistics = updateStatistics.updateStatistics;
 
 
